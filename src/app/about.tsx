ca3a0624a444bcd5c2e62dce0f83187719bfb76f
ORIVALDO DE HOLANDA GAMA <53544978+orivaldogama@users.noreply.github.com>
2023-07-31 17:58:55 -0300

    Add files via upload

---
+++ src/app/about.tsx
@@ -0,0 +1,9 @@
+import { View , Text } from "react-native";
+
+export default function About(){
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontSize: 44, fontWeight: '700'}}>About</Text>
+        </View>
+    )
+}
